8a7d5469ba48a2b13e3b313d6114caf1e5d11cdc
張暐翔 <112915564+markoo0706@users.noreply.github.com>
2023-06-19 22:51:32 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,48 @@
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+
+#line token
+channel_access_token = 'fkoo8cmH1C29XiX7vKOqcXa3fJ5wpcHDZkkMw9Y6v7sxhIeT2QZW/VoE1legG4KY6ZaxTXjgtjKc9M9hyZ6oI+KlGbyUUQejNB17GKyMNrQcMwEHpSq7kI0ibsYn6bZO33jExHJ30qGPd+cXp8G6tgdB04t89/1O/w1cDnyilFU='
+channel_secret = '14a2ec07a0be3dceff5b6cefcbf60b03'
+line_bot_api = LineBotApi(channel_access_token)
+handler = WebhookHandler(channel_secret)
+
+app = Flask(__name__)
+
+# 監聽所有來自 /callback 的 Post Request
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return 'OK'
+@handler.add(MessageEvent, message=TextMessage)
+
+def handle_message(event):
+    
+    if event.message.type == "text": # 若使用者傳送文字訊息，echo 
+        msg= event.message.text
+        message = TextSendMessage(text = msg)
+        line_bot_api.reply_message(event.reply_token,message)
+    elif event.message.type == "location": # 若使用者傳送位址資訊，則回傳經緯度、地址資訊。
+        lat = event.message.latitude
+        long = event.message.longtitude
+        add = event.message.address
+        msg = f"親愛的用戶您好，以下是您的位置資訊：\n緯度：{lat}\n經度：{long}\n地址：{add}" # 回傳訊息
+        message = TextSendMessage(text = msg)
+        line_bot_api.reply_message(event.reply_token,message)
+
+
+import os
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
