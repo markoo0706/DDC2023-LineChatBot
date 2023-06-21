583db6593334152e71a36e9c1cbaed0bf8eafe40
張暐翔 <112915564+markoo0706@users.noreply.github.com>
2023-06-22 01:35:54 +0800

    123

--- mongodb.py
+++ mongodb.py
@@ -1,89 +1,89 @@
-import pymongo
-import pandas as pd
+# import pymongo
+# import pandas as pd
 
 
-myclient = pymongo.MongoClient('mongodb://mongo:463R2dK98HNM@infra.zeabur.com:30774')
+# myclient = pymongo.MongoClient('mongodb://mongo:463R2dK98HNM@infra.zeabur.com:30774')
  
-mydb = myclient['userDB']
+# mydb = myclient['userDB']
 
-record_col = mydb["record"] # 餐廳紀錄 Collection
-favorite_col = mydb["favorite"] # 收藏名單 Collection
-rest_col = mydb["restaurant"] # 餐廳資料 Collection
+# record_col = mydb["record"] # 餐廳紀錄 Collection
+# favorite_col = mydb["favorite"] # 收藏名單 Collection
+# rest_col = mydb["restaurant"] # 餐廳資料 Collection
 
-# 測試
-df = pd.read_pickle("/Users/markoo/Downloads/data.pkl")
+# # 測試
+# df = pd.read_pickle("/Users/markoo/Downloads/data.pkl")
 
-def add_favrite(userId, rest: dict): # 新增收藏紀錄
-    favorite_col.insert_one({
-        "userID" : userId,
-        "Info" : rest
-    })
-    print("新增了一項收藏名單")
-    return
+# def add_favrite(userId, rest: dict): # 新增收藏紀錄
+#     favorite_col.insert_one({
+#         "userID" : userId,
+#         "Info" : rest
+#     })
+#     print("新增了一項收藏名單")
+#     return
 
-def add_record(userId, rest: dict): # 新增餐廳紀錄
-    record_col.insert_one({
-        "userID" : userId,
-        "Info" : rest
-        })
-    print("您已新增了一筆資料 !")
-    return True
+# def add_record(userId, rest: dict): # 新增餐廳紀錄
+#     record_col.insert_one({
+#         "userID" : userId,
+#         "Info" : rest
+#         })
+#     print("您已新增了一筆資料 !")
+#     return True
 
-def delete_info(userId): # 刪除某用戶的歷史紀錄
-    myquery = { "userID": userId}
-    record_col.delete_many(myquery)
-    print("您已刪除了一筆資料 !")
-    return True
+# def delete_info(userId): # 刪除某用戶的歷史紀錄
+#     myquery = { "userID": userId}
+#     record_col.delete_many(myquery)
+#     print("您已刪除了一筆資料 !")
+#     return True
 
-def get_all_user_info(colName):
-    for x in colName.find():
-        print(x["Info"])
-    return True
+# def get_all_user_info(colName):
+#     for x in colName.find():
+#         print(x["Info"])
+#     return True
 
-def delete_collection(colName):
-    for x in colName.find():
-        colName.delete_one(x)
-    print("您刪除了所有資料 !")
-    return True
+# def delete_collection(colName):
+#     for x in colName.find():
+#         colName.delete_one(x)
+#     print("您刪除了所有資料 !")
+#     return True
 
-# 測試 飲食紀錄 資料庫
-def test_record():
-    # 新增資料
-    add_record("使用者A", df["亞廬義大利窯烤吃到飽餐廳"])
-    add_record("使用者B", df["Onni韓食堂"])
-    add_record("使用者C", df["SUBWAY 敦化和平店"])
-    add_record("使用者A", df["三顧茅廬台北四維店"])
-    add_record("使用者C", df["北平同慶樓"])
-    print()
-    # 刪除使用者資料 
-    delete_info("使用者B")
-    print()
-    # 印出所有者資料
-    get_all_user_info(record_col)
+# # 測試 飲食紀錄 資料庫
+# def test_record():
+#     # 新增資料
+#     add_record("使用者A", df["亞廬義大利窯烤吃到飽餐廳"])
+#     add_record("使用者B", df["Onni韓食堂"])
+#     add_record("使用者C", df["SUBWAY 敦化和平店"])
+#     add_record("使用者A", df["三顧茅廬台北四維店"])
+#     add_record("使用者C", df["北平同慶樓"])
+#     print()
+#     # 刪除使用者資料 
+#     delete_info("使用者B")
+#     print()
+#     # 印出所有者資料
+#     get_all_user_info(record_col)
 
-    # 刪除所有使用者資料
-    delete_collection(record_col)
-    return True
+#     # 刪除所有使用者資料
+#     delete_collection(record_col)
+#     return True
 
-# 測試 用戶偏好資料庫
-def test_favorite():
+# # 測試 用戶偏好資料庫
+# def test_favorite():
 
-    add_favrite("使用者A", df["亞廬義大利窯烤吃到飽餐廳"])
-    add_favrite("使用者B", df["Onni韓食堂"])
-    add_favrite("使用者C", df["SUBWAY 敦化和平店"])
-    add_favrite("使用者A", df["三顧茅廬台北四維店"])
-    add_favrite("使用者C", df["北平同慶樓"])
+#     add_favrite("使用者A", df["亞廬義大利窯烤吃到飽餐廳"])
+#     add_favrite("使用者B", df["Onni韓食堂"])
+#     add_favrite("使用者C", df["SUBWAY 敦化和平店"])
+#     add_favrite("使用者A", df["三顧茅廬台北四維店"])
+#     add_favrite("使用者C", df["北平同慶樓"])
 
-    # 印出所有使用者資料
-    get_all_user_info(favorite_col)
+#     # 印出所有使用者資料
+#     get_all_user_info(favorite_col)
 
-    # 刪除所有資料
-    delete_collection(favorite_col)
-    return
+#     # 刪除所有資料
+#     delete_collection(favorite_col)
+#     return
 
-print(mydb.list_collection_names())
+# print(mydb.list_collection_names())
 
-# test_record()
-test_favorite()
+# # test_record()
+# # test_favorite()
 
 
