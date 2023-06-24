b8d45c68bc504a9f5ede5a1f320827b4f2cd18e3
張暐翔 <112915564+markoo0706@users.noreply.github.com>
2023-06-24 12:40:19 +0800

    新增圖案

--- main.py
+++ main.py
@@ -121,7 +121,7 @@ def handle_text_message(event):
             template=CarouselTemplate(
                 columns=[
                     CarouselColumn(
-                        thumbnail_image_url='https://steam.oxxostudio.tw/download/python/line-template-message-demo.jpg',
+                        thumbnail_image_url='https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference=AZose0kJlqfj84bg8gfxcZktpL_2a22UWyzMKmWFHHVuzsu-2vn9avw_5aWhLkfM5zoTvogt8zeb-bOVtW-Vwcy1f-BZZXCuE353J1QzHKmT3iAqmjk_XJYDiXjiP7dsI7Mig-z16In7vq_jiVlW7FuvFwInXMhZvB2TQE8JucLHq5vO8NwU&key=AIzaSyAfxiZ36COzkAF__lM05Er6teR2fYMmZog',
                         title='莫宰羊-大安台大店(4.2)',
                         text= '106台灣台北市大安區新生南路三段28號',
                         actions=[
@@ -136,7 +136,7 @@ def handle_text_message(event):
                         ]
                     ),
                     CarouselColumn(
-                        thumbnail_image_url='https://steam.oxxostudio.tw/download/python/line-template-message-demo2.jpg',
+                        thumbnail_image_url='https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference=AZose0ni5ewMZfHJQ0567H2r-e9hSDcSez7c8w3ucLpsARE_F-nOPl72UJ59ugehI1sVeSPci3K2MHRIQ4B1CHwtF_P1KlFSq86249_TopTqabRiHuWUZEKJTL21a8zhMRaTsIkJZR2IYKKwWDoJdO2dJTbGc96PnYBpxw45XosLlCTxTcGg&key=AIzaSyAfxiZ36COzkAF__lM05Er6teR2fYMmZog',
                          title= '小李子清粥小菜(3.8)',
                         text= '106台灣台北市大安區復興南路二段142之1號',
                         actions=[
@@ -151,7 +151,7 @@ def handle_text_message(event):
                         ]
                     ),
                     CarouselColumn(
-                        thumbnail_image_url='https://steam.oxxostudio.tw/download/python/line-template-message-demo2.jpg',
+                        thumbnail_image_url='https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference=AZose0lIpUTLHhL0KWcECRrYZWKderXX_XrapcmnWhHTkq_Jf6-wW-txGge9d_muMK6KIsJsUMWhSLx6gmAn_c4DZssS9Lec94sLDD0Zq08g39_zR-6jncN1dtjoYNfAGIe30H_I_efpLieE7_LBiKg5U9bpYHg46et67H61LutaymCYNwjP&key=AIzaSyAfxiZ36COzkAF__lM05Er6teR2fYMmZog',
                         title= '北平同慶樓(4.0)',
                         text= '106台灣台北市大安區敦化南路二段168號',
                         actions=[
@@ -166,7 +166,7 @@ def handle_text_message(event):
                         ]
                     ),
                     CarouselColumn(
-                        thumbnail_image_url='https://steam.oxxostudio.tw/download/python/line-template-message-demo2.jpg',
+                        thumbnail_image_url='https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference=AZose0kyMI7znwnJGf7CpNLGiQfZp8GhDlcEzv74PFuaNbbpaPQpKMvMcXZHJfdMqdClQ_peWJvE5yAkC9xSP4Tvw3l5RLLhqZv7BDGUAfX4SyD9N4ylR-fn7UgH2Nr3A380CCFWTMxk5o4fU9tI9QKEbI5mjQGfYXawfA3xnDHG_qNrRbI&key=AIzaSyAfxiZ36COzkAF__lM05Er6teR2fYMmZog',
                         title= '阿玉水餃 (生水餃專賣店)(4.3)',
                         text= '106台灣台北市大安區辛亥路二段217號',
                         actions=[
@@ -190,7 +190,7 @@ def handle_text_message(event):
             template=CarouselTemplate(
                 columns=[
                     CarouselColumn(
-                        thumbnail_image_url='https://steam.oxxostudio.tw/download/python/line-template-message-demo.jpg',
+                        thumbnail_image_url='https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference=AZose0kbiSOBLqu3PjxT544LET1pgdi4WFH6Bt4kJ6MY9hcGSdUK3BmC-HCvHvHqPNHD-swhFcLQjEXaJ5S90Cphtatc8TPflf5g5RNW603WMhcPddXFlfnQco9pAbscx6cLRi8VI0HqbDUs5hIZPTZ7JnUC6C8AoROFJVSwPoqxh3-_DjDR&key=AIzaSyAfxiZ36COzkAF__lM05Er6teR2fYMmZog',
                         title= '鐵匠 鉄板居酒屋 TEPPAN IZAKAYA TESSHO(4.2)',
                         text= '106台灣台北市大安區敦化南路二段265巷13號',
                         actions=[
@@ -205,7 +205,7 @@ def handle_text_message(event):
                         ]
                     ),
                     CarouselColumn(
-                        thumbnail_image_url='https://steam.oxxostudio.tw/download/python/line-template-message-demo2.jpg',
+                        thumbnail_image_url='https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference=AZose0k8tsyE-at1uJcgRFi6_xxHP-vN4aptDd00M37Z3Unf0Dbd3hAA4FQptyWpNkyiFzqxLA9u9-PJ4AqIbvaCZnFFgjWX7UE7_M2KOfaF-u7UPrFxoNKLEcCBTINYY0AYHqIy_7aAam7gHrjB0lrkHu1MIH3Z2dU0EXEEj4ZrOV7dXkLA&key=AIzaSyAfxiZ36COzkAF__lM05Er6teR2fYMmZog',
                          title= 'ibuki 日本料理餐廳 -台北遠東香格里拉(4.4)',
                         text= '106台灣台北市大安区敦化南路二段201號7樓',
                         actions=[
@@ -220,7 +220,7 @@ def handle_text_message(event):
                         ]
                     ),
                     CarouselColumn(
-                        thumbnail_image_url='https://steam.oxxostudio.tw/download/python/line-template-message-demo2.jpg',
+                        thumbnail_image_url='https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference=AZose0mZzxF_pqtOL8x5uePd9-qDINvCYGQGuOC-sMGk0HmIIEjdkY7TTIKbDQrOdTRFtia7S0Bexx6AY0Z25g3VrOzr1wgiFTNTap1m6Swqq_QHb3hg21nHAxBkX0vQ7bSDM7hXHWlCKdLwKJnUjg1XwqCmhEGUR47t4wqeoRscNjYVbL6p&key=AIzaSyAfxiZ36COzkAF__lM05Er6teR2fYMmZog',
                         title= '禾豐日式涮涮鍋(4.4)',
                         text= '106台灣台北市大安區復興南路二段148巷16號1樓',
                         actions=[
@@ -235,7 +235,7 @@ def handle_text_message(event):
                         ]
                     ),
                     CarouselColumn(
-                        thumbnail_image_url='https://steam.oxxostudio.tw/download/python/line-template-message-demo2.jpg',
+                        thumbnail_image_url='https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference=AZose0l4hIBvmAy7lRHOhGIjpR4P54kjrGq1cJUYvR6SQnYZFxvjeFGnR9JAhYCUbtmf74mQ6L_AERPgCfCvoYRvhd_MWQIZNT7lMdL2_zR-RzYpy5cdNhLlt7E8c93LHEGQC4JTTMGCfM2Y7vfFuMFFbj9FM9OQ_WSUi0DcnYHuFwqSrdPh&key=AIzaSyAfxiZ36COzkAF__lM05Er6teR2fYMmZog',
                         title= '角屋關東煮(4.2)',
                         text= '106台灣台北市大安區大安路二段141巷',
                         actions=[
@@ -259,7 +259,7 @@ def handle_text_message(event):
             template=CarouselTemplate(
                 columns=[
                     CarouselColumn(
-                        thumbnail_image_url='https://steam.oxxostudio.tw/download/python/line-template-message-demo.jpg',
+                        thumbnail_image_url='https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference=AZose0l569DhxMcrdWgufVWxlkqEPEDr61i7Ys_FGCaM8Y7M7d4wMeAuRBHW0lCzK1iZXBux0G0DJ1930qdo7v2cJLYGc08e00y_l1LOQ3uZFTO1otLRlkm6DjfkdaXs63PrTfq1KJudzSvDwzcnGec0iV_UyvjgrhaoePB53L5iYW3-9dFg&key=AIzaSyAfxiZ36COzkAF__lM05Er6teR2fYMmZog',
                         title= 'the Diner 樂子瑞安店(4.3)',
                         text= '106台灣台北市大安區瑞安街145號',
                         actions=[
@@ -274,7 +274,7 @@ def handle_text_message(event):
                         ]
                     ),
                     CarouselColumn(
-                        thumbnail_image_url='https://steam.oxxostudio.tw/download/python/line-template-message-demo2.jpg',
+                        thumbnail_image_url='https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference=AZose0mnTXvqo_RiA4D9PJnGar0n65c1-Zrw1iEts-4KPyOzWdWCfZmmGsXkhkLcj7Uzoh-rrS0LHkO5D3f3ZBMsy_6NtNxjlrcCurrN02K5QDxEHYxtUL_rFqg8oIjSgAb4sQkGltFmDpqsf5g_fICQytGS18VVXYjDagnZ-4NuO2l_fet8&key=AIzaSyAfxiZ36COzkAF__lM05Er6teR2fYMmZog',
                         title='遠東CAFÉ(4.5)',
                         text= '106台灣台北市大安區和平東路三段60號',
                         actions=[
@@ -289,7 +289,7 @@ def handle_text_message(event):
                         ]
                     ),
                     CarouselColumn(
-                        thumbnail_image_url='https://steam.oxxostudio.tw/download/python/line-template-message-demo2.jpg',
+                        thumbnail_image_url='https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference=AZose0kPQjYrRtN6xCc6WMld_xbZ9z1Dz3pZSxOrnEthJ6VkeN8IcYhMA20KN2B60YXI9W9XxHRxVk0UICSxkEHyklTmSHpSNYaIqc1Sdxqi8DOiHM-1SUD4tjrfreIJjiq4FZObN9Cb1U96ridN77_t-TD6bEiX-EZwLPxNEyQrmX7eV7Iw&key=AIzaSyAfxiZ36COzkAF__lM05Er6teR2fYMmZog',
                         title='Onni韓食堂(3.8)',
                         text='106台灣台北市大安區復興南路二段173號',
                         actions=[
@@ -304,7 +304,7 @@ def handle_text_message(event):
                         ]
                     ),
                     CarouselColumn(
-                        thumbnail_image_url='https://steam.oxxostudio.tw/download/python/line-template-message-demo2.jpg',
+                        thumbnail_image_url='https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference=AZose0kypXa9rs804I-hm_PBIaDmYWT8IijS3KEzIUZRPTfYVsvpNPToWXpXP57nU2dCNZJ1zam6YsFJRmthYqM0hyaO_QZtQjRL9wblstsoE_x71z66yMg8Mwjc0q_g4P3SSI8KLxQsO5OccpmN3GW-d0I0HpZ0fe6qig7sVukXqxiACN91&key=AIzaSyAfxiZ36COzkAF__lM05Er6teR2fYMmZog',
                         title='找餐。店(3.8)',
                         text='106台灣台北市大安區和平東路三段1巷6-3號',
                         actions=[
