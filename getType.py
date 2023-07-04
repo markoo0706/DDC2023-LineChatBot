a8a18c850ae11b9b1ddf126bee370ed5eb546ca4
cwhsu023 <75806504+cwhsu023@users.noreply.github.com>
2023-07-04 23:08:19 +0800

    Create getType.py

---
+++ getType.py
@@ -0,0 +1,12 @@
+import geopy.distance
+
+def getTypeApi():
+    r = requests.get("https://testapi.zeabur.app/result")
+    recommend_list = json.loads(r.text)
+    return recommend_list
+
+def getType(lat, lng):
+  recommend_list = getTypeApi()
+  dist_list = [[geopy.distance.geodesic((lat, lng), (x['lat'], x['lng'])).km, x['recommend']] for x in recommend_list]
+  type = min(dist)[1].split()
+  return type[0][2:], type[1][2:], type[2][2:]
